a0d2a64223f1a72c230e0d385dd0c7a9265aaa62
jacobcwright <jacob.wright.ut@gmail.com>
2022-04-04 14:05:14 -0600

    began person activity, but debugging search activity startup

---
+++ app/src/main/java/edu/byu/jwrig30/familymapclient/personActivity/PersonActivity.java
@@ -0,0 +1,16 @@
+package edu.byu.jwrig30.familymapclient.personActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.byu.jwrig30.familymapclient.R;
+
+public class PersonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+}
