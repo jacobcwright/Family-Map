c4e3a1f18b2db2cc781a894ae76c2ea771db3ab9
jacobcwright <jacob.wright.ut@gmail.com>
2022-04-10 19:10:28 -0600

    added tests for login

--- app/src/test/java/edu/byu/jwrig30/familymapclient/ModelTest.java
+++ app/src/test/java/edu/byu/jwrig30/familymapclient/ModelTest.java
@@ -5,8 +5,56 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ModelTest {
-        @Test
-        public void addition_isCorrect() {
-            assertEquals(4, 2 + 2);
-        }
+    @Test
+    public void calculateRelationshipPass(){
+
+    }
+
+    @Test
+    public void calculateRelationshipFail(){
+
+    }
+
+    @Test
+    public void filterEventsPass(){
+
+    }
+
+    @Test
+    public void filterEventsFail(){
+
+    }
+
+    @Test
+    public void sortEventsPass(){
+
+    }
+
+    @Test
+    public void sortEventsFail(){
+
+    }
+
+    @Test
+    public void searchPeoplePass(){
+
+    }
+
+    @Test
+    public void searchPeopleFail(){
+
+    }
+
+    @Test
+    public void searchEventPass(){
+
+    }
+
+    @Test
+    public void searchEventFail(){
+
+    }
+
+
+
 }
--- app/src/test/java/edu/byu/jwrig30/familymapclient/ServerProxyTest.java
+++ app/src/test/java/edu/byu/jwrig30/familymapclient/ServerProxyTest.java
@@ -1,12 +1,76 @@
 package edu.byu.jwrig30.familymapclient;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import edu.byu.jwrig30.familymapclient.server.ServerProxy;
+import request.LoginRequest;
+import result.LoginResult;
+
 public class ServerProxyTest {
-        @Test
-        public void addition_isCorrect() {
-            assertEquals(4, 2 + 2);
-        }
+    @Test
+    public void loginPass() {
+        System.out.println("Running loginPass");
+        ServerProxy server = new ServerProxy();
+        LoginRequest req = new LoginRequest("sheila", "parker");
+        LoginResult result = server.login( req, "127.0.0.1", "8080");
+
+        Assert.assertTrue(result.getSuccess());
+    }
+
+    @Test
+    public void loginFail() {
+        System.out.println("Running loginFail");
+
+        ServerProxy server = new ServerProxy();
+        LoginRequest req = new LoginRequest("sheila", "WRONGPASSWORDLOLXD");
+        LoginResult result = server.login( req, "127.0.0.1", "8080");
+
+        Assert.assertFalse(result.getSuccess());
+    }
+
+    @Test
+    public void registerPass() {
+        System.out.println("Running registerPass");
+
+
+    }
+
+    @Test
+    public void registerFail() {
+        System.out.println("Running registerFail");
+
+
+    }
+
+    @Test
+    public void retrievePeoplePass() {
+        System.out.println("Running retrievePeoplePass");
+
+
+    }
+
+    @Test
+    public void retrievePeopleFail() {
+        System.out.println("Running retrievePeopleFail");
+
+
+    }
+
+    @Test
+    public void retrieveEventsPass() {
+        System.out.println("Running retrieveEventsPass");
+
+
+    }
+
+    @Test
+    public void retrieveEventsFail() {
+        System.out.println("Running retrieveEventsFail");
+
+
+    }
 }
